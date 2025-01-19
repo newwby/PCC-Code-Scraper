c1316930ae771a7c79183b79b99d250223dd781e
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2025-01-19 20:36:56 +0000

    response plain text not json

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const PORT = 4000;
 app.get('/home', (req, res) => {
   const postcode = req.query.postcode
   if (postcode) {
-    res.status(200).json(`Welcome, your app is working well, thanks for querying ${postcode} but I don't know what to do with that yet.`);
+    res.status(200).send(`Welcome, your app is working well, thanks for querying ${postcode} but I don't know what to do with that yet.`);
   } else {
-    res.status(400).json('Welcome, your app is working well, but you forgot to include a postcode.');
+    res.status(400).send('Welcome, your app is working well, but you forgot to include a postcode.');
   }
 });
 
