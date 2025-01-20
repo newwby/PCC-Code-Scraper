c846412f31e716d4690a39b2f7a46bd44bb7e69d
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2025-01-20 21:51:05 +0000

    bugfix pre-flight issues

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const app = express();
-const PORT = 4000;
+const PORT = process.env.PORT || 3000;
 
+// pre-flight
+app.options('*', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.status(200).send('');
+});
+
+// standard behaviour
 app.get('/', (req, res) => {
-  const postcode = req.query.postcode
+  const postcode = req.query.postcode;
   if (postcode) {
     res.status(200).send(`Thanks for querying ${postcode}, ON THE DEV BRANCH!, but I don't know what to do with that yet.`);
   } else {
     res.status(400).send('Welcome, your app is working well, but you forgot to include a postcode.');
   }
+});
+
+// Set CORS headers for GET requests as well
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
 });
 
 app.listen(PORT, () => {
