76fdc8394f21ac852bdcb5bca6e6dc4d0e91661e
DanDoesAThing <60653155+newwby@users.noreply.github.com>
2025-01-27 11:50:00 +0000

    re-enabled scraper

--- index.js
+++ index.js
@@ -12,13 +12,9 @@ app.get('/home', async (req, res) => {
 app.get('/', async (req, res) => {
   const postcode = req.query.postcode
   if (postcode) {
-    res.status(400).send('Due to a deployment error with Chromium functionality is temporarily suspended - please check back again soon!');
-    // Temporary disabled functionality
-    /*
     const pcc_code_output = await scraper.get_code(postcode, true)
     const parsed_output = parser.parseScraperOutput(pcc_code_output)
     res.status(200).send(`${parsed_output}`);
-    */
   } else {
     res.status(400).send('Welcome, your app is working well, but you forgot to include a postcode.');
   }
